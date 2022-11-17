0d406725071efa847e05011854ed1beb5378480c
dio-p <dionempapadop@gmail.com>
2022-11-17 09:39:15 +0000

    have started working on the schema

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ import cors from 'cors'
 import { MongoClient }  from 'mongodb';
 
 app.use(cors());
-import createRouter from './helpers/create_router.js';
+import createRouter from './helpers/createRouter.js';
 
 app.use(express.json());
 export let db;
 let eventsCollection;
 let unitsCollection;
 let artifactsCollection
-export let apiRouter;
+export let appsRouter;
 
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
 .then(client => {
   db = client.db('big_picture');
-  eventsCollection = db.collection('events');
-  unitsCollection = db.collection('units');
-  artifactsCollection = db.collection('artifacts');
-  apiRouter = createRouter(eventsCollection, unitsCollection, artifactsCollection);
-  app.use('/api', apiRouter);
+  appsCollection = db.collection('events');
+  appsRouter = createRouter(appsCollection);
+  app.use('/api', appsRouter);
 })
   app.listen(5051, function(){
     console.log(`app listening on port ${this.address().port}`);
---
+++ mongo/appsSeed.js
@@ -0,0 +1,39 @@
+use appsDb
+db.dropDatabase();
+
+db.apps.insertMany([
+    {
+        name: "Optimo",
+        type: "App",
+        briefDescr: "this is the optimo app, the best app in the world",
+        depResposible: ["DPub"],
+        connection: [
+          {
+          name: "Things-api",
+          id: "someString",
+          typeOf: "resievesFrom"
+          },
+          {
+            name: "Disco-api",
+            id: "someString",
+            typeOf: "resievesFrom"
+          }
+        ],
+        docs: [
+          {
+            title: "Some Doc1",
+            link: "https://someLink.com",
+            linkSource: "Confluence",
+            linkCreationDate: "someDate",
+            isLinkUpToDate: true
+          },
+          {
+            title: "Some Doc2",
+            link: "https://someLink.com",
+            linkSource: "Confluence",
+            linkCreationDate: "someDate",
+            isLinkUpToDate: false
+          }
+        ]
+      }
+])
