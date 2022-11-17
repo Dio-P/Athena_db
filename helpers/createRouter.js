ade0fc72fcea3c6a439e17b95c71c10af5b9fcc4
dio-p <dionempapadop@gmail.com>
2022-11-17 11:11:14 +0000

    the post to root is adding an app

--- helpers/createRouter.js
+++ helpers/createRouter.js
@@ -4,6 +4,45 @@ import express from 'express';
 // import { db } from '../index.js';
 // import fetch from 'node-fetch';
 
+const mockAppTest = {
+  name: "Optimo2",
+  type: "App",
+  briefDescr: "this is the optimo app, the best app in the world",
+  depResposible: ["DPub"],
+  facing: {
+    user: true,
+    audience: false,
+  },
+  connection: [
+    {
+    name: "Things-api",
+    id: "someString",
+    typeOf: "resievesFrom"
+    },
+    {
+      name: "Disco-api",
+      id: "someString",
+      typeOf: "resievesFrom"
+    }
+  ],
+  docs: [
+    {
+      title: "Some Doc1",
+      link: "https://someLink.com",
+      linkSource: "Confluence",
+      linkCreationDate: "someDate",
+      isLinkUpToDate: true
+    },
+    {
+      title: "Some Doc2",
+      link: "https://someLink.com",
+      linkSource: "Confluence",
+      linkCreationDate: "someDate",
+      isLinkUpToDate: false
+    }
+  ]
+}
+
 
 const createRouter = function (appsCollection) {
     const router = express.Router();
@@ -20,36 +59,36 @@ const createRouter = function (appsCollection) {
       });
     });
   
-  //   router.get('/events/:title', (req, res) => {
-  //     const title = req.params.title;
-  //     appsCollection
-  //     .findOne({ title: title })
-  //     .then((doc) => {
-  //       console.log("doc", doc);
-  //       return res.json(doc);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //       res.status(500);
-  //       res.json({ status: 500, error: err });
-  //     });
-  //   });
+    router.get('/apps/:name', (req, res) => {
+      const name = req.params.name;
+      appsCollection
+      .findOne({ name: name })
+      .then((doc) => {
+        console.log("doc", doc);
+        return res.json(doc);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+    });
   
-  //   // Newer version of Mongodb put request - doesnt give back ops object anymore
-  //   router.post('/', (req, res) => {
-  //     const newData = req.body;
-  //     appsCollection
-  //     .insertOne(newData)
-  //     .then((result) => {
-  //       return eventsRouter.findOne({ _id: result.insertedId })
-  //     })
-  //     .then((doc) => res.json(doc))
-  //     .catch((err) => {
-  //       console.error(err);
-  //       res.status(500);
-  //       res.json({ status: 500, error: err });
-  //     });
-  //   });
+    router.post('/', (req, res) => {
+      const newApp = mockAppTest;
+      // const newData = req.body;
+      appsCollection
+      .insertOne(newApp)
+      .then((result) => {
+        return appsCollection.findOne({ _id: result.insertedId })
+      })
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+    });
   
   //   router.delete('/:id', (req, res) => {
   //     const id = req.params.id;
