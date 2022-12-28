01b20599312e6cdf64cc59848c0c14c9439160e5
dio-p <dionempapadop@gmail.com>
2022-12-28 11:44:46 +0000

    working on the index hopefully having a draft of the structure

---
+++ graphQl/resolvers.js
@@ -0,0 +1,7 @@
+function createResolvers() {
+  return {
+    Query: {
+
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import bodyParser from 'body-parser';
 
 import express from 'express';
 const app = express();
@@ -14,12 +15,19 @@ export let db;
 let appsCollection
 let appsRouter;
 
+const graphqlUrl = '/graphql';
+const apolloServer = createApolloServer();
+
+app.use(graphqlUrl, bodyParser.json({ limit: '50mb' }));
+apolloServer.applyMiddleware({ app });
+
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
 .then(client => {
   db = client.db('apps_db');
   appsCollection = db.collection('apps');
   appsRouter = createRouter(appsCollection);
-  app.use('/athena', appsRouter);
+  // app.use('/athena', appsRouter);
+  app.use('/athena', apolloServer);//attempt to link this
 })
   app.listen(5051, function(){
     console.log(`app listening on port ${this.address().port}`);
