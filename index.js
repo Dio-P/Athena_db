2cc4adbb0861ffba795de9ffaa8578bbbf06aab9
dio-p <dionempapadop@gmail.com>
2022-12-29 14:31:31 +0000

    tidied up the index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import bodyParser from 'body-parser';
 import { createApolloServer } from './graphQl/apolloServerFactory.js';
-import http from 'http';
 import { MongoClient }  from 'mongodb';
 import { expressMiddleware } from '@apollo/server/express4';
-
-
+import http from 'http';
 import express from 'express';
 import cors from 'cors'
+import bodyParser from 'body-parser';
 
 const graphqlUrl = '/graphql';
 export let db;
-export let appsCollection
+export let appsCollection;
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -20,11 +16,6 @@ const apolloServer = createApolloServer(httpServer);
 
 await apolloServer.start();
 
-// app.use(cors());
-import createRouter from './helpers/createRouter.js';
-
-// app.use(express.json());
-// let appsRouter;
 app.use(
   graphqlUrl,
   cors(),
@@ -34,17 +25,10 @@ app.use(
   }),
 );
 
-// app.use(graphqlUrl, bodyParser.json({ limit: '50mb' }));
-// apolloServer.applyMiddleware({ app });
-
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
 .then(client => {
   db = client.db('apps_db');
   appsCollection = db.collection('apps');
-  // appsRouter = createRouter(appsCollection);
-  // app.use('/athena', appsRouter);
-  // app.use('/athena', apolloServer);//attempt to link this
-  // how to pass appsCollection within apolloServer?
 })
 
 await new Promise((resolve) => httpServer.listen({ port: 5051 }, resolve));
