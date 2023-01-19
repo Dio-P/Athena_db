254ccf4b9dcc376e8bcc7817b0034fe23ebd399e
dio-p <dionempapadop@gmail.com>
2023-01-19 12:21:16 +0000

    tidied upthe parts Model file. About to add the docs in it

--- graphQl/models/partsModel.js
+++ graphQl/models/partsModel.js
@@ -22,35 +22,13 @@ const toPart = (app, partId) => {
   
 }
 
-
-
-// const toPart = (app, partId) => {
-//   const {
-//     parts,
-//     properties
-//   } = app;
-//   console.log("$parts", parts);
-//   // return 
-//   return {
-//     parts: parts.find((part)=>(part.id===partId)),
-//     properties
-//   }
-
 export function PartsModel() {
   return {
   
-    async getPartByIdAndAppId(args){
-      console.log("args.partId", args.partId);
-      console.log("args.appId", args.appId);
-      const dbRes = await appsCollection.findOne({ "parts.id": "somePartId1" });
-      // const dbRes = await appsCollection.aggregate([{$unwind: "$parts" }]).toArray();
-      // const dbRes = await appsCollection.find({ "app.parts": {$elemMatch: {"app.parts.id": "somePartId1"} }}).toArray();
-      // am I messing up the ids? (local and db- here I need the db)
-      console.log("dbRes", dbRes);
-      console.log("test@");
-      let app = toPart(dbRes, args.partId);
-      console.log("app", app);
-      return app
+    async getPartByIdAndAppId({partId, appId}){
+      const dbRes = await appsCollection.findOne({ "parts.id": partId });
+      let part = toPart(dbRes, partId);
+      return part
     },
 
     // async getAppByName(args){
